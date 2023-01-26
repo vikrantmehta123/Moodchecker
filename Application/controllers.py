400891367ce0c141b80ebde39471baf153046361
Vikrant Mehta <vikrantmehta123@gmail.com>
2023-01-26 10:58:13 +0530

    added the edit page, fixed bug

--- Application/controllers.py
+++ Application/controllers.py
@@ -1,6 +1,7 @@
 from main import app
-from flask import render_template, request, session, redirect, make_response, abort, url_for
+from flask import render_template, request, session, redirect, make_response, abort, url_for, jsonify
 from Models.models import *
+from utils import *
 
 CLIENT_ID = app.CLIENT_ID
 app = app.app
@@ -45,13 +46,11 @@ def register(admin_status):
                 }
                 session['admin'] = admin
                 admin_status = 1
-                return redirect(request.url, admin_status=1)
+                return redirect(url_for("register", admin_status=1))
         else:
-            data = request.form.to_dict()
-            print(f"data is {data}")
-            print("admin status is not 0")
+            print(request.form.to_dict())
             if 'admin' in session:
-                print(f"session is {session['admin']}")
+                pass
             return render_template("register.html")
 
 @app.route("/moods", methods=["GET", "POST"])
@@ -59,12 +58,43 @@ def moods():
     if request.method == "GET":
         return render_template("moods.html")
     
-@app.route("/edit")
+@app.route("/login/<int:sender_code", methods=["GET", "POST"])
+def login(sender_code):
+    if request.method == "GET":
+        return render_template("login.html")
+    else:
+        # TODO: Implement logic for checking whether the logged in user is a valid admin
+        token = request.form["credential"]
+        admin = google_login_authentication(token)
+        if admin:
+            session['admin'] = admin
+        else:
+            return "There was an error with the login"
+
+        # If the request for login came from the edit page, redirect the user to the edit page
+        if sender_code == 2:
+            return redirect(url_for("edit"))
+        elif sender_code == 3:
+            return redirect(url_for('reports'))
+
+@app.route("/edit", methods=["GET", "POST"])
 def edit():
-    return render_template("edit.html")
+    if 'admin' not in session:
+        return redirect(url_for('login', sender_code=2))
+    if request.method == "GET":
+        # TODO: Implement a function to get family members of from the data
+        members = []
+        return render_template("edit.html", members=members)
+    elif request.method == "POST":
+        # TODO: Implement a function to update the database for the changes in entries
+        pass
 
 @app.route("/reports", methods=["GET","POST"])
 def reports():
-    if request.method == "POST":
-        print(request.form.to_dict())
-        return render_template("reports.html")
+    if 'admin' not in session:
+        return redirect(url_for('login', sender_code=3))
+
+    if request.method == "GET":
+        # TODO: Implemnt a function to get the last month's data
+        report = []
+        return render_template("reports.html", report= report)
